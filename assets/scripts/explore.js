eb055ffb1fc7235aed5330b5c81465b4ea5ba193
riasinghania <73354668+riasinghania@users.noreply.github.com>
2025-05-05 14:57:02 -0700

    Update explore.js

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -3,5 +3,46 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  const synth = window.speechSynthesis;
+  const voiceSelect = document.getElementById('voice-select');
+  const speakButton = document.querySelector('button');
+  const textInput = document.getElementById('text-to-speak');
+  const faceImage = document.querySelector('img');
+
+  let voices = [];
+
+  function populateVoices() {
+    voices = synth.getVoices();
+    voiceSelect.innerHTML = '';
+
+    voices.forEach((voice) => {
+      const option = document.createElement('option');
+      option.textContent = `${voice.name} (${voice.lang})`;
+      option.value = voice.name;
+      voiceSelect.appendChild(option);
+    });
+  }
+
+  // Load voices list (some browsers load them asynchronously)
+  populateVoices();
+  if (speechSynthesis.onvoiceschanged !== undefined) {
+    speechSynthesis.onvoiceschanged = populateVoices;
+  }
+
+  speakButton.addEventListener('click', () => {
+    const utterThis = new SpeechSynthesisUtterance(textInput.value);
+    const selectedVoice = voiceSelect.value;
+
+    utterThis.voice = voices.find((voice) => voice.name === selectedVoice);
+
+    // Change face to open-mouth
+    faceImage.src = 'assets/images/smiling-open.png';
+
+    // Revert face after speaking
+    utterThis.onend = () => {
+      faceImage.src = 'assets/images/smiling.png';
+    };
+
+    synth.speak(utterThis);
+  });
 }
